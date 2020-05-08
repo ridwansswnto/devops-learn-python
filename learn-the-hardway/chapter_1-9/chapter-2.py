14beea4d148e1e399a3ffc6196fab0c940ec3d0e
Ridwan Dwi Siswanto <ridwansiswanto@Ridwan-Siswanto.local>
2020-05-08 10:12:12 +0700

    learn(python-the-hardway): about comment

---
+++ learn-the-hardway/chapter_1-9/chapter-2.py
@@ -0,0 +1,11 @@
+# Ini comment
+# Ini tidak akan di Print
+# anyting behind # is ignored by python
+
+print("I could have code like this.") #comment
+
+# comment biasanya di gunakan untuk menjelaskan suatu block code
+
+# print("This won't run")
+
+print("This will run")
