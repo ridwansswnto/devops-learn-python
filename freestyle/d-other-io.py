f3ad3aa29160ddd9a397b5b6196d82d023c54987
Ridwan Dwi Siswanto <ridwansiswanto@Ridwan-Siswanto.local>
2020-05-09 13:20:58 +0700

    learn(freestyle): more about i/o

---
+++ freestyle/d-other-io.py
@@ -0,0 +1,8 @@
+# Python progmram to get input from user
+
+# accepting integer from the user
+num1 = int(input("Enter num-1: "))
+num2 = int(input("Enter num-2: "))
+
+num3 = num1 * num2
+print("Product is:", num3)
