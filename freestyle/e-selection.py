3e1ee1b907a226cb6949447e1fa08407aaf5fa93
Ridwan Dwi Siswanto <ridwansiswanto@Ridwan-Siswanto.local>
2020-05-09 13:33:01 +0700

    learn(freestyle): about selection condition

---
+++ freestyle/e-selection.py
@@ -0,0 +1,9 @@
+# Python program to illustrate selection statement
+
+num1 = 34
+if(num1 > 12):
+    print("Num1 is good")
+elif(num1 > 35):
+    print("Num1 is not goood..")
+else:
+    print("Num1 is great")
