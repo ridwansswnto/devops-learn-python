1b86739efdcd9b7e2fd43dc3b6f3ff0b712e766a
Ridwan Dwi Siswanto <ridwansiswanto@Ridwan-Siswanto.local>
2020-05-11 05:14:55 +0700

    learn(freestyle): math module

---
+++ freestyle/i-modules.py
@@ -0,0 +1,13 @@
+# Python porogram to illustrate math module
+import math
+
+
+def Main():
+    num = float(input('Enter a number: '))
+
+    num = math.fabs(num)
+    print(num)
+
+
+if __name__ == "__main__":
+    Main()
