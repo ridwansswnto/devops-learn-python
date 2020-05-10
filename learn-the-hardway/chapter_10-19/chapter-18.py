39a3ddde42e11d645ec1a9fd399ac985392c7a10
Ridwan Dwi Siswanto <ridwansiswanto@Ridwan-Siswanto.local>
2020-05-10 17:07:35 +0700

    learn(python-the-hardway): simple function

--- learn-the-hardway/chapter_10-19/chapter-18.py
+++ learn-the-hardway/chapter_10-19/chapter-18.py
@@ -1,1 +1,21 @@
 def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+
+def print_none():
+    print("I go nothin'.")
+
+
+print_two("Ridwan", "Siswanto")
+print_two_again("Ridwan", "Siswanto")
+print_one("First!")
+print_none()
