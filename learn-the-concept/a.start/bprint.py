aba9bb84f454cd611f69154ff7769fa085f28eab
Ridwan Dwi Siswanto <ridwan.siswnto@gmail.com>
2020-05-17 03:08:50 +0700

    learn(python-the-concept): again classic pring

---
+++ learn-the-concept/a.start/bprint.py
@@ -0,0 +1,26 @@
+palindrome = "A man, \nA plan, \nA canal, \nPanama."
+print(palindrome)
+
+tabbing = """
+\tabc
+a\tbc
+ab\tc
+"""
+# \ is escape everthing charater behind that
+print(tabbing)
+
+# combine string using +
+nama = ("Ridwan " + "Dwi " + "Siswanto")
+print(nama)
+print("=" * 10)
+
+fname = "Ridwan"
+tname = "Dwi"
+lname = "Siswanto"
+print(fname + tname + lname)
+print(fname, tname, lname)
+
+# Get character
+huruf = "abcdefghijklmnopqrstuvwxyz"
+print('{} {} {} {} {} {}'.format(huruf[17], huruf[8], huruf[3], huruf[22], huruf[0], huruf[13]))
+# print(huruf[17][8][3][22][0][13])
