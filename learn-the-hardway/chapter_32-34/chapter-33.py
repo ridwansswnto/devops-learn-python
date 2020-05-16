e35ea294ac269e6a933ba0018d43d0c2b83d3a96
Ridwan Dwi Siswanto <ridwan.siswnto@gmail.com>
2020-05-16 07:08:03 +0700

    learn(python-the-hardway): while loop

--- learn-the-hardway/chapter_32-34/chapter-33.py
+++ learn-the-hardway/chapter_32-34/chapter-33.py
@@ -1,12 +1,15 @@
 i = 0
 numbers = []
 
-while i < 6:
+while i < 3:
+    print("+======+")
     print(f"At the top i is {i}")
     numbers.append(i)
 
     i = i + 1
+    print("-----")
     print("Numbers now: ", numbers)
+    print("-----")
     print(f"At the bottom i is {i}")
 
 print("The numbers: ")
