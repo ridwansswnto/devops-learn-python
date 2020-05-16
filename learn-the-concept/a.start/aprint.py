d93dbdd0389acf14e5fc0cc0d5a5cdfcb0df91ba
Ridwan Dwi Siswanto <ridwan.siswnto@gmail.com>
2020-05-17 02:22:01 +0700

    learn(python-the-concept): a classic print

---
+++ learn-the-concept/a.start/aprint.py
@@ -0,0 +1,26 @@
+print("Hello World")
+print("=" * 10, "\n")
+
+print("'Nay!', said naysayer. 'Neigh?' said the horse.")
+print("=" * 10, "\n")
+
+print('The rare double quote in capacity: ".')
+print("=" * 10, "\n")
+
+print('A "two by four" is actually 1 1/2" x 3 1/2".')
+print("=" * 10, "\n")
+
+print("'There's the man that shot my paw!' cried the limping hound.")
+print("=" * 10, "\n")
+
+print('''Boom!''')
+print("""Boom!""")
+print("=" * 10, "\n")
+
+poem = ''' 
+Sebuah kota yang indah, Bandung
+Terdapat berbagai wisata berada disana.
+Seperti suasana yang sejuk di lembang dan ciater.
+Serta mall di daerah Cihampelas
+'''
+print(poem)
