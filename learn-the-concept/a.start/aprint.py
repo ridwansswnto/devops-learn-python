8955a4bbf9e4950448f443254a2b471eb575324c
Ridwan Dwi Siswanto <ridwan.siswnto@gmail.com>
2020-05-17 02:34:46 +0700

    learn(python-the-concept): a classic print

--- learn-the-concept/a.start/aprint.py
+++ learn-the-concept/a.start/aprint.py
@@ -24,3 +24,29 @@ Seperti suasana yang sejuk di lembang dan ciater.
 Serta mall di daerah Cihampelas
 '''
 print(poem)
+
+#String with str
+satu = str(72)
+dua = str(1.231)
+tiga = str(True)
+empat = 72
+
+print("The original string:", satu)
+check = isinstance(satu, str)
+print("Is variable a string? :", check)
+print("=" * 10, "\n")
+
+print("The original string:", dua)
+check = type(dua) == str
+print("Is variable a string?:", check)
+print("=" * 10, "\n")
+
+print("The original string:", tiga)
+check = isinstance(tiga, str)
+print("Is variable a string?:", check)
+print("=" * 10, "\n")
+
+print("The original string:", empat)
+check = isinstance(empat, str)
+print("Is variable a string? :", check)
+print("=" * 10, "\n")
