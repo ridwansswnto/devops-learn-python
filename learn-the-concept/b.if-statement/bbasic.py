e6b01385c7169abb028220e152b9e72d31948ac8
Ridwan Dwi Siswanto <ridwan.siswnto@gmail.com>
2020-05-17 23:25:37 +0700

    learn(python-the-concept): basic if statements

---
+++ learn-the-concept/b.if-statement/bbasic.py
@@ -0,0 +1,26 @@
+color = "mauve"
+
+if color == "red":
+    print("It's an apple")
+elif color == "green":
+    print("It's green pepper")
+elif color == "been purple":
+    print("I don't know what it is, but only bees can see it")
+else:
+    print("I've never heard of the color", color)
+
+print("=" * 8)
+# it is true?
+some_list = []
+if some_list:
+    print("There's something in here.")
+else:
+    print("Heyy!, it's empty!!")
+
+print("=" * 8)
+# it is true?
+some_list = [""]
+if some_list:
+    print("There's something in here.")
+else:
+    print("Heyy!, it's empty!!")
