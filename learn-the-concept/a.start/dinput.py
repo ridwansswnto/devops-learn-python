60fdbe85d0151806d55ea3788282b1814a181715
Ridwan Dwi Siswanto <ridwan.siswnto@gmail.com>
2020-05-17 15:38:18 +0700

    learn(python-the-concept): input user

---
+++ learn-the-concept/a.start/dinput.py
@@ -0,0 +1,14 @@
+name = input("What's your name? ")
+print(name)
+
+#Input always return a string
+value1 = input("Value 1: ")
+value2 = input("Value 2: ")
+all_value = value1 + value2
+print(f"Gabungan dari value1 dan value2 :", all_value)
+
+#Input with integer
+value1 = int(input("Value 1: "))
+value2 = int(input("Value 2: "))
+all_value = value1 + value2
+print(f"Jumlah dari value1 + value2: ", all_value)
