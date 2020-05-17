a4ebf7c7962104ea064a70fcbd08e1260e953bc4
Ridwan Dwi Siswanto <ridwan.siswnto@gmail.com>
2020-05-18 02:50:55 +0700

    learn(python-the-concept): lists basic

--- learn-the-concept/d.tuples-list/blists.py
+++ learn-the-concept/d.tuples-list/blists.py
@@ -3,3 +3,10 @@ print(animals)
 
 #Accessing elements in a list
 print(animals[3], animals[2])
+
+name = list("RIDWAN")
+print(name)
+
+atuple = ("I", "Love", "Icecream")
+alist = list(atuple)
+print(alist)
